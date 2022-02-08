3a2a9e28eda3c1099d47e5a221bd8e706a0c5046
contrabrand <86887091+contrabrand@users.noreply.github.com>
2022-02-08 19:20:36 +0800

    Update twk.php

--- twk.php
+++ twk.php
@@ -1,1 +1,61 @@
 
+{"Version":"1.1",
+
+"Url":"https://github.com/contrabrand/twk/raw/main/TT.zip" ,
+
+"Changelog":"  
+
+          🙏 ยินดีต้อนรับเข้าสู่แอพ​ TwinkleTunneling​ 🙏
+                    By NayuTech 
+
+                  เร็ว  นิ่ง​  ปลอดภัย
+  
+โหมดทดสอบ
+
+✅วันที่อัปเดท 
+ 
+
+✅เพิ่ม​ไฟล์​ : 
+
+
+✅เซิฟเวอร์​ : 🇸🇬 🇹🇭  🇺🇸 🇭🇰 🇯🇵 🇰🇷 🇮🇳 🇳🇱
+
+
+✅เครือข่าย​ที่รองรับ
+
+🟢 AIS 🇹🇭 
+🔵 DTAC 🇹🇭
+🔴 TRUE 🇹🇭 
+🟡 DIGI 🇲🇾 🔜
+
+✅โปรที่สามารถเชื่อมต่อได้
+
+🟢 AIS
+ -​ 64 kbps + AISPLAY
+ - โปรรายเดือน
+🔴 TRUE
+ - โปร​ Youtube
+ - โปร​ เฟซบุ๊ค​เกมมิ่ง
+ -​ ไม่มีโปร
+🔵 DTAC
+ - โปรลาซา​ด้า​
+ - โปร​ 64 Kbps + LINE
+ - ไม่มีโปร
+
+✅ สนใจเช่าติดต่อแอดมิน​ LINE​: yallaaaj
+
+",
+
+"latestVersion": "1.0.0",
+
+"latestVersionCode": "1",
+
+"url": "",
+
+ "releaseNotes": [
+
+ "-Demo"
+
+  ]
+
+}
